81ab374d8631502f657413258eae3f42dd3337ae
Nattapat <san.1234@hotmail.com>
2018-10-07 13:59:04 +0700

    Initial Commit

---
+++ Chapter 1 Button chang wordTests/Chapter_1_Button_chang_wordTests.swift
@@ -0,0 +1,36 @@
+//
+//  Chapter_1_Button_chang_wordTests.swift
+//  Chapter 1 Button chang wordTests
+//
+//  Created by Nattapat on 7/10/2561 BE.
+//  Copyright © 2561 Nattapat. All rights reserved.
+//
+
+import XCTest
+@testable import Chapter_1_Button_chang_word
+
+class Chapter_1_Button_chang_wordTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
